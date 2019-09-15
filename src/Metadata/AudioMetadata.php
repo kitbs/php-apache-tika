bf87a97c36b6d16b83c76d6fe292d6a28bc70808
Kit Burton-Senior <mail@kitbs.com>
2019-09-15 18:13:01 +0100

    add fields for audio

--- src/Metadata/AudioMetadata.php
+++ src/Metadata/AudioMetadata.php
@@ -2,6 +2,9 @@
 
 namespace Vaites\ApacheTika\Metadata;
 
+use DateTime;
+use DateTimeZone;
+
 /**
  * Metadata class for audio files
  *
@@ -10,11 +13,90 @@ namespace Vaites\ApacheTika\Metadata;
 class AudioMetadata extends Metadata
 {
     /**
+     * Title
+     *
+     * @var string
+     */
+    public $title = null;
+
+    /**
+     * Artist
+     *
+     * @var string
+     */
+    public $artist = null;
+
+    /**
+     * Album
+     *
+     * @var string
+     */
+    public $album = null;
+
+    /**
+     * Album artist
+     *
+     * @var string
+     */
+    public $albumArtist = null;
+
+    /**
+     * Composer
+     *
+     * @var string
+     */
+    public $composer = null;
+
+    /**
+     * Release year
+     *
+     * @var int
+     */
+    public $releaseYear = null;
+
+    /**
+     * Disc number
+     *
+     * @var int
+     */
+    public $discNumber = null;
+
+    /**
+     * Track number
+     *
+     * @var int
+     */
+    public $trackNumber = null;
+
+    /**
+     * Compilation
+     *
+     * @var bool
+     */
+    public $compilation = false;
+
+    /**
+     * Two-letter language code (ISO-639-1)
+     *
+     * @link https://en.wikipedia.org/wiki/ISO_639-1
+     *
+     * @var string
+     */
+    public $language = null;
+
+    /**
      * Audio duration in seconds.
      *
      * @var int
      */
     public $duration = 0;
+
+    /**
+     * Software used to generate document
+     *
+     * @var string
+     */
+    public $generator = null;
 
     /**
      * Sets an attribute
@@ -26,14 +108,73 @@ class AudioMetadata extends Metadata
      */
     protected function setAttribute($key, $value)
     {
+        $timezone = new DateTimeZone('UTC');
+
         switch(mb_strtolower($key))
         {
             case 'title':
                 $this->title = $value;
                 break;
 
-            case 'duration':
-                $this->duration = (int) $value;
+            case 'xmpdm:artist':
+                $this->artist = $value;
+                break;
+
+            case 'xmpdm:albumartist':
+                $this->albumArtist = $value;
+                break;
+
+            case 'xmpdm:album':
+                $this->album = $value;
+                break;
+
+            case 'xmpdm:composer':
+                $this->composer = $value;
+                break;
+
+            case 'language':
+                $this->language = mb_substr($value, 0, 2);
+                break;
+
+            // case 'duration':
+            case 'xmpdm:duration':
+                $this->duration = (int) ($value / 1000); // FIXME Milliseconds or seconds?
+                break;
+
+            case 'xmpdm:releaseDate':
+                $this->releaseYear = (int) $value;
+                break;
+
+            case 'xmpdm:discnumber':
+                $this->discNumber = (int) $value;
+                break;
+
+            case 'xmpdm:tracknumber':
+                $this->trackNumber = (int) $value;
+                break;
+
+            case 'xmpdm:compilation':
+                $this->compilation = (bool) $value;
+                break;
+
+            case 'creation-date':
+            case 'date':
+                $value = preg_replace('/\.\d+/', 'Z', $value);
+                $this->created = new DateTime($value, $timezone);
+                break;
+
+            case 'last-modified':
+                $value = preg_replace('/\.\d+/', 'Z', $value);
+                $this->updated = new DateTime($value, $timezone);
+                break;
+
+            case 'content-type':
+                $mime = $value ? preg_split('/;\s+/', $value) : [];
+                $this->mime = array_shift($mime);
+                break;
+
+            case 'xmp:creatortool':
+                $this->generator = trim($value);
                 break;
 
             default:
